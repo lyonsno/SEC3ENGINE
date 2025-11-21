9a8625514784682b7b12b0fdaaec60f557e395c6
Noah Lyons <n.lyons53@gmail.com>
2025-11-20 22:25:10 -0500

    Fix particle demo dependencies and guards

--- Sec3Engine/js/core/camera.js
+++ Sec3Engine/js/core/camera.js
@@ -6,10 +6,20 @@
 //SEC3 is a core function interface
 var SEC3 = SEC3 || {};
 
+// Camera type constants expected by legacy demos.
+var CAMERA_EXPLORING_TYPE = 0;
+var CAMERA_TRACKING_TYPE = 1;
+
 SEC3.Camera = function(){
-   
+
    SEC3.PerspProjector.call( this );
 
 };
-    
+
 SEC3.Camera.prototype = Object.create( SEC3.PerspProjector.prototype );
+
+// Convenience factory used by demo pages.
+SEC3.createCamera = function(/* type */){
+    // Only one camera type is implemented; ignore the requested type for now.
+    return new SEC3.Camera();
+};
